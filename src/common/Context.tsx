c6bf1518121f83a26e52bb00bcf1b5834aca1680
Kirill <trkir21@gmail.com>
2025-11-18 20:16:41 +0700

    change context

--- src/common/Context.tsx
+++ src/common/Context.tsx
@@ -3,20 +3,18 @@ import React, { createContext, PropsWithChildren, useContext } from "react";
 export class Context<T> {
   private context: React.Context<T>;
 
-  constructor(defaultValue: any) {
+  constructor(defaultValue: T) {
     this.context = createContext(defaultValue);
-    useContext(this.context);
-    return new Proxy(this, {
-      apply: (_target, _thisArg, args) => {
-        const [props] = args as [PropsWithChildren<{ value: T }>];
-        const Provider = this.context.Provider;
-
-        return <Provider value={props.value}>{props.children}</Provider>;
-      },
-    });
   }
 
-  getContext() {
+  Provider({ children, value }: PropsWithChildren<{ value: T }>) {
+    useContext(this.context);
+    return (
+      <this.context.Provider value={value}>{children}</this.context.Provider>
+    );
+  }
+
+  getContext(): React.Context<T> {
     return this.context;
   }
 }
