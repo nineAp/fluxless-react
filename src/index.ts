dfd4041e61bbdaa4c323e33fde549b65265cbb9b
Kirill <trkir21@gmail.com>
2025-11-22 13:18:11 +0700

    version and exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./common/Context";
 export * from "./common/createSliceMap";
 export * from "./common/Store";
+export * from "./common/hooks/useAction";
+export * from "./common/hooks/useObserver";
